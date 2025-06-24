c9b3736c37b8b2a63374fc0d2e3a5338dbb3422c
Max Ainson <gamemega2546@gmail.com>
2025-06-25 02:22:55 +0700

    Add studentprofile&changepassword

--- src/dashboard/dashboard-common/DashboardBannerTwo.tsx
+++ src/dashboard/dashboard-common/DashboardBannerTwo.tsx
@@ -1,7 +1,6 @@
 //import { Link } from "react-router-dom"
 //import BtnArrow from "../../svg/BtnArrow"
-import InjectableSvg from "../../hooks/InjectableSvg"
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 const apiUrl = import.meta.env.VITE_API_URL || process.env.REACT_APP_API_URL;
